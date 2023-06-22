99e05acbb14d28c4999b8ca348169f96098a2701
Prem <shindeprem102@gmail.com>
2023-06-22 21:04:47 +0530

    issues fixed

--- index.php
+++ index.php
@@ -35,8 +35,6 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true)
     <!-- links -->
 
 
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"> -->
 
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
